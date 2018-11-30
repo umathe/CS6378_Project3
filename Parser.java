2038f39fefecedf083f2ef041c25ff1a0388845a
umathe <uxt130030@utdallas.edu>
2018-11-29 18:37:46 -0600

    Add files via upload

--- Parser.java
+++ Parser.java
@@ -45,6 +45,7 @@ public class Parser {
 			boolean firstlinepassed = false;
 			int lineCount = 0;
 			String[] temp_splitarr;
+			String temp_neighbors = "";
 			
 			BufferedReader br = new BufferedReader(new FileReader(config_file));
 			String temp_filerow;
@@ -65,12 +66,15 @@ public class Parser {
 						temp_splitarr = temp_filerow.trim().split(" +"); // line will consist of four tokens
 						
 						num_nodes = Integer.parseInt(temp_splitarr[0].trim()); // number of nodes
-						info_nodes = new String[num_nodes][3];
+						info_nodes = new String[num_nodes][4];
 						
 						mean_inter_request_delay = Integer.parseInt(temp_splitarr[1].trim()); 
 						mean_cs_exe_time = Integer.parseInt(temp_splitarr[2].trim());
 						num_req = Integer.parseInt(temp_splitarr[3].trim()); // number of requests each node should generate
-						
+												
+						for(int i = 0; i<num_nodes; i++) {
+							temp_neighbors += i + " "; //temp var to capture node neighbors for later function						
+						}
 						firstlinepassed = true;
 
 					} else { // first line has passed
@@ -81,7 +85,8 @@ public class Parser {
 						info_nodes[lineCount][0] = temp_splitarr[0].trim(); // node ID 
 						info_nodes[lineCount][1] = temp_splitarr[1].trim() + ".utdallas.edu"; // host name
 						info_nodes[lineCount][2] = temp_splitarr[2].trim(); // port
-						
+						info_nodes[lineCount][3] = temp_neighbors.replace(info_nodes[lineCount][0], "").replaceAll(" +", " ").trim(); // all other nodes are neighbors 
+												
 						lineCount++;					
 					}
 				}
