9637ffd375cb9fc0534a5f99a5d558ebc02a53fe
Anshika Singh <32585104+Anshika-Singh@users.noreply.github.com>
2018-12-12 11:48:58 -0600

    Update Requests.java

--- roucairolCaravalho/Requests.java
+++ roucairolCaravalho/Requests.java
@@ -1,17 +1,21 @@
-package roucairolCaravalho;
-
 import java.net.Socket;
 
 public class Requests {
     public Socket soc; 
     public long ts; 
+    public boolean reqStatus;
     
     public Requests(Socket soc, long ts) { 
         this.soc = soc; 
         this.ts = ts; 
-    } 
+        this.reqStatus = false;
+    }
       
     public Socket getRequest() { 
         return soc; 
-    }  
+    } 
+	
+	public long getTS() { 
+        return ts; 
+    } 
 }
